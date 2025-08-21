ed7c3603484007b411323ac11ad7b7114516d04f
oscarbvf <oscarbvf@gmail.com>
2025-08-21 14:13:30 -0300

    Update tests and policies

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,10 @@
 class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
   def index?
     true
   end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "test1@example.com", password: "123456")
+    @post = Post.create!(titulo: "My first post", conteudo: "Post content")
+    @comment = Comment.new(body: "Comment for testing purposes", post: @post, user: @user)
+  end
+
+  test "Valid comment" do
+    assert @comment.valid?
+  end
+
+  test "Not valid without content" do
+    @comment.body = nil
+    assert_not @comment.valid?
+  end
+
+  test "Not valid without a Post" do
+    @comment.post = nil
+    assert_not @comment.valid?
+  end
+
+  test "Not valid without a User" do
+    @comment.user = nil
+    assert_not @comment.valid?
+  end
 end
