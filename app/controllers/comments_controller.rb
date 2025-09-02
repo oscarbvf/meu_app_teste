8c80a192f76e8396b434546fceac89c4e787225b
Oscarbvf <oscarbvf@gmail.com>
2025-09-02 00:02:32 -0300

    WIP: Implemented Turbo/Stimulus for Comments (Login pending)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,51 +12,78 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.turbo_stream
-        format.html { redirect_to @post, notice: "Comment added" }
+        format.html { redirect_to @post, notice: "Comment added." }
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(
-          dom_id(@post, :new_comment),
-          partial: "comments/form",
-          locals: { post: @post, comment: @comment }
-        )}
-        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            dom_id(@post, :new_comment),
+            partial: "comments/form",
+            locals: { post: @post, comment: @comment }
+          ), status: :unprocessable_entity
+        end
+        format.html do
+          render partial: "comments/form",
+                locals: { post: @post, comment: @comment },
+                status: :unprocessable_entity
+        end
       end
     end
   end
 
   def edit
+    @comment = @post.comments.find(params[:id])
   end
 
   def update
+    # if @comment.update(comment_params)
+    #  respond_to do |format|
+    #    format.turbo_stream do
+    #      render turbo_stream: turbo_stream.replace(
+    #        dom_id(@comment),
+    #        partial: "comments/comment",
+    #        locals: { comment: @comment }
+    #      )
+    #    end
+    #    format.html { redirect_to @post, notice: "Comment updated successfully." }
+    #  end
+    # else
+    #  respond_to do |format|
+    #    format.turbo_stream do
+    #      render turbo_stream: turbo_stream.replace(
+    #        dom_id(@comment),
+    #        partial: "comments/form",
+    #        locals: { post: @post, comment: @comment }
+    #      )
+    #    end
+    #    format.html { render :edit, status: :unprocessable_entity }
+    #  end
+    # end
+    @comment = @post.comments.find(params[:id])
+
     if @comment.update(comment_params)
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(
-            dom_id(@comment),
-            partial: "comments/comment",
-            locals: { comment: @comment }
-          )
-        end
+        format.turbo_stream
         format.html { redirect_to @post, notice: "Comment updated successfully." }
       end
     else
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(
-            dom_id(@comment),
-            partial: "comments/form",
-            locals: { post: @post, comment: @comment }
-          )
-        end
+        format.turbo_stream { render :edit, status: :unprocessable_entity }
         format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
+    # @comment.destroy
+    # respond_to do |format|
+    #  format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@comment)) }
+    #  format.html { redirect_to @post, notice: "Comment deleted." }
+    # end
+    @comment = @post.comments.find(params[:id])
     @comment.destroy
+
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@comment)) }
+      format.turbo_stream
       format.html { redirect_to @post, notice: "Comment deleted." }
     end
   end
