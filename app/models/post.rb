b03588d1a7d1792aa528da31e7b0947633e1891e
oscarbvf <oscarbvf@gmail.com>
2025-09-08 10:52:31 -0300

    WIP: starting broadcast implementation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   validates :conteudo, presence: true
 
   has_many :comments, dependent: :destroy
+
+  # Send automatically for all listeners
+  after_create_commit -> { broadcast_append_to "posts" }
+  after_update_commit -> { broadcast_replace_to "posts" }
+  after_destroy_commit -> { broadcast_remove_to "posts" }
 end
